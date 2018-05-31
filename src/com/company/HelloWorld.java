0b26f799e25d75bdd31d89d5337a60077dbe704e
GBTC440007UR <GBTC440007UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-05-31 14:39:58 -0400

    first commit

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println( "Hello world!" );
+        System.out.println( "Today is May 31, 2018. I am alive!" );
+        System.out.println( "My name is Juyeon" );
+    }
+}
